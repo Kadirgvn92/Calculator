b3af22b97b8be2a7102b016478b6135a80647ace
Kadir <kdrgvndeveloper@gmail.com>
2023-11-25 16:16:24 +0300

    Hello I created a simple calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputOne: UITextField!
+    @IBOutlet weak var inputTwo: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
-
-
+    
+    
+    @IBAction func btnCalculate(_ sender: Any) {
+        if let firstNum = Int(inputOne.text!) {
+            if let secondNum = Int(inputTwo.text!){
+                let result = firstNum + secondNum
+                lblResult.text = String(result)
+            }
+        }
+    }
+    
+    
+    @IBAction func btnMinus(_ sender: Any) {
+        if let firstNum = Int(inputOne.text!) {
+            if let secondNum = Int(inputTwo.text!){
+                let result = firstNum - secondNum
+                lblResult.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func btnMulti(_ sender: Any) {
+        if let firstNum = Int(inputOne.text!) {
+            if let secondNum = Int(inputTwo.text!){
+                let result = firstNum * secondNum
+                lblResult.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func btnDivide(_ sender: Any) {
+        if let firstNum = Int(inputOne.text!) {
+            if let secondNum = Int(inputTwo.text!){
+                let result = firstNum / secondNum
+                lblResult.text = String(result)
+            }
+        }
+    }
+    
 }
 
